5a356c43c8adf14b28b7f9b3b0499b09ad319e95
kumakichi <xyesan@gmail.com>
2018-05-18 14:29:11 +0800

    skeleton

--- main.go
+++ main.go
@@ -3,24 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 )
 
 func main() {
+	if runtime.GOOS != "linux" {
+		fmt.Println("Currently, support only linux platform")
+		os.Exit(0)
+	}
+
 	if len(os.Args) < 2 {
 		usage()
 	}
+	cmd := os.Args[1]
+	args := os.Args[2:]
 
-	switch os.Args[1] {
+	switch cmd {
 	case "get":
-		get()
+		get(args)
 	case "list":
 		list()
 	case "del":
-		delete()
+		del(args)
 	case "set":
-		set()
+		set(args)
 	case "conn":
-		connect()
+		conn(args)
 	default:
 		fmt.Printf("Unsupported command: %s\n", os.Args[1])
 		os.Exit(-1)
@@ -28,6 +36,6 @@ func main() {
 }
 
 func usage() {
-	fmt.Printf("Usage: %s [get/set/list/delete/connect] [group]\n", os.Args[0])
+	fmt.Printf("Usage: %s [get/set/list/del/connect] [group]\n", os.Args[0])
 	os.Exit(0)
 }
--- op.go
+++ op.go
@@ -1,23 +1,42 @@
 package main
 
-import "os/exec"
+import (
+	"fmt"
+	"os"
+)
 
-func get() {
-
+func get(args []string) {
+	if len(args) != 1 {
+		errArgs("Should be like: %s get groupName\n", os.Args[0])
+	}
 }
 
 func list() {
 
 }
 
-func delete() {
-
+func del(args []string) {
+	if len(args) < 1 {
+		errArgs("Should be like: %s del group1 [group2] ...\n", os.Args[0])
+	}
 }
 
-func set() {
-
+func set(args []string) {
+	if len(args) < 2 {
+		errArgs("Should be like: %s set groupName host=someHost user=someUser ...\n", os.Args[0])
+	}
 }
 
-func connect() {
-	exec.Command("mysql", "--defaults-group-suffix")
+func conn(args []string) {
+	if len(args) != 1 {
+		errArgs("Should be like: %s conn groupName\n", os.Args[0])
+	}
+
+	fmt.Printf("mysql --defaults-group-suffix=%s\n", args[0])
+}
+
+func errArgs(format string, a ...interface{}) {
+	fmt.Println("Invalid arg[s]")
+	fmt.Printf(format, a...)
+	os.Exit(-1)
 }
